89aa9a5ba9e825c01174396e3e20f717ade5bba0
sarahcrisley <sarah.risley1@maine.edu>
2022-12-07 10:53:29 -0500

    Adding indices calculations

--- UNTB_Comp_Models.R
+++ UNTB_Comp_Models.R
@@ -105,7 +105,6 @@ plot(sp10$tstep, sp10$abund, type = 'b',
 
 
 
-
 ##################
 ###PURTUBATIONS###
 ##################
@@ -188,8 +187,54 @@ plot(sort(abundPost$abund[abundPost$tstep == max(abundPost$tstep)],
      xlab = 'Rank', ylab = 'Abundance', main = 'Long after invasion')
 
 
+##Getting final SAD's for time steps 
+preSADinvas <- abundPre$abund[abundPre$tstep == max(abundPre$tstep)]
+postSADinvas <- abundPost$abund[abundPost$tstep == 13000]
+longSADinvas <- abundPost$abund[abundPost$tstep == max(abundPost$tstep)]
 
 
+##################
+###HILL NUMBERS###
+##################
+
+#hill package 
+library(hillR)
+
+# Evenness 
+q <- 2 #Simpsons evenness 
+
+neut_2 <- hillR::hill_taxa(finalSADneut, q)
+comp_2 <- hillR::hill_taxa(finalSADComp, q)
+pre_2 <- hillR::hill_taxa(preSADinvas, q)
+post_2 <- hillR::hill_taxa(postSADinvas, q)
+long_2 <- hillR::hill_taxa(longSADinvas, q)
+
+q <- 0 #Species richness 
+
+neut_0 <- hillR::hill_taxa(finalSADneut, q)
+comp_0 <- hillR::hill_taxa(finalSADComp, q)
+pre_0 <- hillR::hill_taxa(preSADinvas, q)
+post_0 <- hillR::hill_taxa(postSADinvas, q)
+long_0 <- hillR::hill_taxa(longSADinvas, q)
 
 
+q <- 1-(1e-5) #Shannon's Index 
+neut_1 <- hillR::hill_taxa(finalSADneut, q)
+comp_1 <- hillR::hill_taxa(finalSADComp, q)
+pre_1 <- hillR::hill_taxa(preSADinvas, q)
+post_1 <- hillR::hill_taxa(postSADinvas, q)
+long_1 <- hillR::hill_taxa(longSADinvas, q)
+
+# q = 0, get species richness 
+# q can't be exactly one, but as q gets close to 1 use 1-1E^-5 (like 1)
+
+##Compiling 
+tab <- matrix(c(neut_2, comp_2, pre_2, post_2,long_2,
+                neut_0, comp_0, pre_0, post_0, long_0,
+                neut_1, comp_1, pre_1, post_1, long_1), ncol=3, nrow = 5)
+colnames(tab) <- c('Simpsons Evenness','Richness', 'Shannons Index')
+rownames(tab) <- c('Neutral Model','Competitive Model', 'Pre-Invasion', 'Post-Invasion', 'After Invasion')
+tab <- as.table(tab)
+tab <- round(tab, digits = 2)
+tab
 
